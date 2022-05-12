3a770b12dff26233097c0916eba1cf634fb97f9d
Gabriel <gabrielalves3654@gmail.com>
2022-05-12 18:05:40 -0300

    Segundo exercicio feito

--- bloco4-conteudo/exercicios.js
+++ bloco4-conteudo/exercicios.js
@@ -1,10 +1,19 @@
-let num1 = 5;
-let num2 = 7;
+//let num1 = 5;
+//let num2 = 7;
 
-console.log (num1 + num2);
-console.log (num2 - num1);
-console.log (num1 * num2);
-console.log (num1 / num2);
-console.log (num1 % num2);
+//console.log(num1 + num2);
+//console.log(num2 - num1);
+//console.log(num1 * num2);
+//console.log(num1 / num2);
+//console.log(num1 % num2);
 
 
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+const num1 = 30;
+const num2 = 24;
+
+if (num1 > num2){
+    console.log(num1)
+} else {
+    console.log(num2)
+}
