af393d8835d12e4ec7583dd3e38dfcf65b1958ee
Gabriel <gabrielalves3654@gmail.com>
2022-05-12 18:42:52 -0300

    Quinto exercicio feito

--- bloco4-conteudo/exercicios.js
+++ bloco4-conteudo/exercicios.js
@@ -36,14 +36,34 @@
 
 
 //4-Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-const Numero = 0;
+//const Numero = 0;
 
-if (Numero == 0){
-    console.log("Negativo")
-} else if  (Numero > 10){
-    console.log("positivo")
+//if (Numero == 0){
+//    console.log("Negativo")
+//} else if  (Numero > 10){
+//    console.log("positivo")
+//} else {
+ //   console.log("Negativo")
+//}
+
+
+
+
+
+
+//-5 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+//Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
+//Um ângulo será considerado inválido se não tiver um valor positivo.
+
+const angulo1 = 130;
+const angulo2 = 30;
+const angulo3 = -10;
+const somaDosAngulos = 180;
+
+if (angulo1 , angulo2 , angulo3 < 0){
+   console.log("ERRO")
+} else if (angulo1 + angulo2 + angulo3 == somaDosAngulos){ 
+   console.log("True")
 } else {
-    console.log("Negativo")
+   console.log("False")
 }
-
-
