e74d1145650b4f9ad0bb77da37172d6b72aab1c4
Gabriel <gabrielalves3654@gmail.com>
2022-05-12 17:26:29 -0300

    condicoes de aprovado/reprovado e recuperacao

---
+++ bloco4-conteudo/scripts.js
@@ -0,0 +1,13 @@
+const notaFinal = 
+
+
+
+if (notaFinal >= 7 && notaFinal <=10){
+    console.log("Voce esta aprovado!");
+} else if (notaFinal < 5 && notaFinal >= 0) {
+    console.log("Voce esta reprovado!");
+} else if (notaFinal >=5 && notaFinal <7) {
+    console.log("Voce esta de recuperacão");
+}   else {
+    console.log ("Numero inválido!");
+}
