0b7e1e1fd9892058219a0f42b5de9a86dfa0d08c
Gabriel <gabrielalves3654@gmail.com>
2022-05-12 18:27:58 -0300

    Quarto exercicio feito

--- bloco4-conteudo/exercicios.js
+++ bloco4-conteudo/exercicios.js
@@ -8,7 +8,7 @@
 //console.log(num1 % num2);
 
 
-//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+//2-Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 //const num1 = 30;
 //const num2 = 24;
 
@@ -20,17 +20,30 @@
 
 
 
-//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
-const num1 = 1000;
-const num2 = 600;
-const num3 = 554;
+//3-Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+//const num1 = 1000;
+//const num2 = 600;
+//const num3 = 554;
 
 
-if (num1 > num2 && num1 > num3){
-    console.log(num1);
-} else if (num2 > num1 && num2 > num3){
-    console.log(num2);
+//if (num1 > num2 && num1 > num3){
+//    console.log(num1);
+//} else if (num2 > num1 && num2 > num3){
+   // console.log(num2);
+//} else {
+//    console.log(num3);
+//}
+
+
+//4-Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+const Numero = 0;
+
+if (Numero == 0){
+    console.log("Negativo")
+} else if  (Numero > 10){
+    console.log("positivo")
 } else {
-    console.log(num3);
+    console.log("Negativo")
 }
+
 
