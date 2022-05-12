d3ef46ab3d0c518e7270301efeba9f8e04912fdc
Gabriel <gabrielalves3654@gmail.com>
2022-05-12 18:10:56 -0300

    terceiro exercicio feito

--- bloco4-conteudo/exercicios.js
+++ bloco4-conteudo/exercicios.js
@@ -9,11 +9,28 @@
 
 
 //Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
-const num1 = 30;
-const num2 = 24;
+//const num1 = 30;
+//const num2 = 24;
 
-if (num1 > num2){
-    console.log(num1)
+//if (num1 > num2){
+//    console.log(num1);
+//} else {
+//    console.log(num2);
+//}
+
+
+
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+const num1 = 1000;
+const num2 = 600;
+const num3 = 554;
+
+
+if (num1 > num2 && num1 > num3){
+    console.log(num1);
+} else if (num2 > num1 && num2 > num3){
+    console.log(num2);
 } else {
-    console.log(num2)
+    console.log(num3);
 }
+
