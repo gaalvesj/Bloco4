86b86d49c26a5b3f824907af231640ef02438c92
Gabriel <gabrielalves3654@gmail.com>
2022-05-13 16:09:03 -0300

    Conteudo de For/of

--- bloco4-conteudo/scripts.js
+++ bloco4-conteudo/scripts.js
@@ -36,14 +36,39 @@
 //console.log(menuServices);
 
 
-let cars = ["saab", "Volvo" , "BMW"]
-for (let index = 0; index < cars.length; index ++){
-    console.log(cars[index])
-}
+//<-----For----->
 
-
+//let cars = ["saab", "Volvo" , "BMW"]
+//for (let index = 0; index < cars.length; index ++){
+//    console.log(cars[index])
+//}
 
 //let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 //for (let index = 0; index < groceryList.length; index++){
 //    console.log(groceryList[index])
+//}
+
+
+//<--------for/of--------->
+
+//let numeros = [1,2,3,4,5,6];
+//for(let numero of numeros){
+//    console.log(numero)
+//}
+
+//let word = 'Hello';
+//for (let index of word){
+//    console.log(index)
+//}
+
+//let arrOfNumbers = [10, 20 , 30];
+//for (let index of arrOfNumbers) {
+//    index ++;
+//    console.log(index);
+//}
+
+//exercicio 1:
+//let names = ["Joao", "Maria", "Antonio", "Margarida"];
+//for (let index of names) {
+//    console.log(index)
 //}
