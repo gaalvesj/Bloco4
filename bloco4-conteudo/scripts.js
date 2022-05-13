7793cbfc7eda12d0e8771ef2f165d39ee1d0774d
Gabriel <gabrielalves3654@gmail.com>
2022-05-13 18:05:47 -0300

    Adiicionando um progama de mega sena simples mas funcional.

--- bloco4-conteudo/scripts.js
+++ bloco4-conteudo/scripts.js
@@ -72,3 +72,34 @@
 //for (let index of names) {
 //    console.log(index)
 //}
+
+
+//----MEGA SENA----
+
+let chatNumbers = [10 , 23 , 26, 18, 50, 49];
+let megaSenaNumbers = [10, 23 ,43 ,44 ,51 ,58]
+
+//console.log("Jogo da galera: ");
+//for (let index = 0; index < chatNumbers.length ; index += 1 ){
+//    console.log (chatNumbers[index]);
+//}
+
+//console.log("Jogo da Mega Senna: ");
+//for (let index2 = 0; index2 < megaSenaNumbers.length ; index2 += 1 ){
+//    console.log (megaSenaNumbers[index2]);
+//}
+
+let numberOfHits = 0;
+for (let index = 0; index < chatNumbers.length ; index += 1 ){
+       console.log (chatNumbers[index]);
+       for (let index2 = 0; index2 < megaSenaNumbers.length ; index2 += 1 ){
+             console.log (megaSenaNumbers[index2]);
+         if (chatNumbers[index] === megaSenaNumbers[index2]){
+             numberOfHits += 1; 
+         }
+    
+        }                    
+}
+console.log("Jogo da galera: ",chatNumbers);
+console.log("Jogo da Mega: ",megaSenaNumbers);
+console.log('Numero de Acertos: ',numberOfHits);
