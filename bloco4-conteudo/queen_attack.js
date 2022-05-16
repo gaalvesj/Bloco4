ad09ee414662c918cdd94373e3d6652e16f2773e
Gabriel <gabrielalves3654@gmail.com>
2022-05-16 17:41:50 -0300

    Adicionando varial da diagonal no codigo

--- bloco4-conteudo/queen_attack.js
+++ bloco4-conteudo/queen_attack.js
@@ -1,22 +1,30 @@
 //posicao da rainha teste
 let linhaRainha = 0;
 let colunaRainha = 0;
-let diagonalRainha = 0;
+
 //posicao do bispo teste
 let linhaBispo = 3;
 let colunaBispo = 3;
-let diagonalBispo = 3;
+
 //identifica se o ataque foi bem sucessido ou nao
 let podeAtacar = false;
 
-// verifica se esta na mesma linha
+// verifica se esta na mesma linha e mesma coluna
  if (linhaRainha === linhaBispo || colunaRainha === colunaBispo){
      podeAtacar = true;
  }
- console.log(podeAtacar);
 
 // verifica se esta na mesma diagonal
-if (diagonalRainha === diagonalBispo){
-    podeAtacar = true;
-} 
-console.log (podeAtacar);
+for (let passos = 1; passos <= 7 ; passos +=1 ){
+    let linhaRainhaClone = linhaRainha + passos;
+    let colunaRainhaClone = colunaRainha + passos;
+    console.log(linhaRainhaClone, colunaRainhaClone)
+
+    if(linhaRainhaClone === linhaBispo && colunaRainhaClone === colunaBispo) {
+        podeAtacar = true;
+        console.log ("Ataque bem sucedido na diagonal.")
+        break;
+    }
+
+}
+console.log(podeAtacar);
