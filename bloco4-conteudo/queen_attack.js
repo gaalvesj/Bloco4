39108dc51eb060ba5e909f1b1a5438d9e8071b18
Gabriel <gabrielalves3654@gmail.com>
2022-05-16 16:48:00 -0300

    Adicionando os primeirps paramentros

--- bloco4-conteudo/queen_attack.js
+++ bloco4-conteudo/queen_attack.js
@@ -6,3 +6,16 @@ let colunaRainha = 0;
 let linhaBispo = 3;
 let colunaBispo = 3;
 
+//identifica se o ataque foi bem sucessido ou nao
+let podeAtacar = false;
+
+// verifica se esta na mesma linha
+ if (linhaRainha === linhaBispo){
+     podeAtacar = true;
+ }
+ console.log(podeAtacar);
+
+//verifica se esta na mesma coluna
+
+
+// verifica se esta na mesma diagonal
