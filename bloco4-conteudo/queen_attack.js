44e345957790496461e8d0f975d39ebafc2d8f87
Gabriel <gabrielalves3654@gmail.com>
2022-05-16 17:01:50 -0300

    Refatorando o codigo para deixar ele mais curto

--- bloco4-conteudo/queen_attack.js
+++ bloco4-conteudo/queen_attack.js
@@ -1,21 +1,22 @@
 //posicao da rainha teste
 let linhaRainha = 0;
 let colunaRainha = 0;
-
+let diagonalRainha = 0;
 //posicao do bispo teste
 let linhaBispo = 3;
 let colunaBispo = 3;
-
+let diagonalBispo = 3;
 //identifica se o ataque foi bem sucessido ou nao
 let podeAtacar = false;
 
 // verifica se esta na mesma linha
- if (linhaRainha === linhaBispo){
+ if (linhaRainha === linhaBispo || colunaRainha === colunaBispo){
      podeAtacar = true;
  }
  console.log(podeAtacar);
 
-//verifica se esta na mesma coluna
-
-
 // verifica se esta na mesma diagonal
+if (diagonalRainha === diagonalBispo){
+    podeAtacar = true;
+} 
+console.log (podeAtacar);
